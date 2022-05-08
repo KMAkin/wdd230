9f66e1c69ddf349124ff7527c402ca4f2c412bee
KMAkin <kris.akin12@gmail.com>
2022-05-07 18:23:36 -0700

    uploading week 3 files

---
+++ week3/scripts/main.js
@@ -0,0 +1,5 @@
+let string = document.lastModified;
+document.querySelector("#last-update").innerHTML=(document.lastModified);
+
+let year = new Date().getFullYear();
+document.querySelector("#year").innerHTML=year
